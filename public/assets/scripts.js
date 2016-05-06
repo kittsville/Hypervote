f0ef9cc891451b96f6fdd8c2993db0832ceb69cd
Kit <kittsville@googlemail.com>
2016-05-06 21:23:47 +0100

    Cleaned up scripts file

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -2,10 +2,6 @@ var data = {
 	labels: [1462552828917, 1462552835917, 1462552833917, 1462552836917, 1462552838917, 1462552840917,],
 	series: [[100, 80, 20, -10, -50, -40]],
 },
-data2 = {
-	labels: [1462552828917, 1462552835917, 1462552833917, 1462552836917, 1462552838917, 1462552840917,],
-	series: [[10, 20, 30, 60, 90, 70]],
-},
 options = {
 	height: 300,
 	high: 100,
@@ -30,18 +26,10 @@ options = {
 			return dateObj.getHours() + ':' + dateObj.getMinutes() + ':' + dateObj.getSeconds();
 		}
 	}
-},
-responsiveOptions = [
-  ['screen and (max-width: 640px)', {
-    showPoint: false,
-  }],
-    ['screen and (max-width: 1024px)', {
-    seriesBarDistance: 5,
-  }]
-];
+};
 
 // Initialize a Line chart in the container with the ID chart1
-var chart = new Chartist.Line('#votes-chart', data, options, responsiveOptions);
+var chart = new Chartist.Line('#votes-chart', data, options);
 
 chart.on('draw', function(event) {
 	// If the draw event is for labels on the x-axis
